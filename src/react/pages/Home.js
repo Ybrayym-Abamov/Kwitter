4d5471f9a3cb07dd465106c8bca31da63e40f450
safia88 <safialiusa@gmail.com>
2019-11-23 11:33:23 -0500

    react comment paternt currection

--- src/react/pages/Home.js
+++ src/react/pages/Home.js
@@ -6,7 +6,7 @@ class Home extends React.Component {
   render() {
     return (
       <>
-        <TopMenu /> //might remove menu from login screen for style purposes
+        <TopMenu /> {/*might remove menu from login screen for style purposes*/}
         <h2>Your favorite microblogging platform</h2>
         <LoginForm />
       </>
