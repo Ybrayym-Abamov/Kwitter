530297e42cf7f977736c99f26938971098827f35
Samantha Kyle <samanthakyle@gmail.com>
2019-11-25 10:00:01 -0500

    adjust top menu to set up for linking between pages

--- src/react/pages/index.js
+++ src/react/pages/index.js
@@ -1,9 +1,14 @@
 import Home from "./Home";
 import Profile from "./Profile";
 import NotFound from "./NotFound";
+import Users from "./Users";
+import Settings from "./Settings";
+
 
 export default {
   Home: { path: "/", component: Home },
   Profile: { path: "/profile/:username", component: Profile },
-  NotFound: { path: "*", component: NotFound }
+  NotFound: { path: "*", component: NotFound },
+  Users: { path: "/users", component: Users },
+  Settings: { path: "/settings", component: Settings }
 };
