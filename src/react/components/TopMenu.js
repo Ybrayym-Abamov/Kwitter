3156b3c12ea541b3c7c290172d10cfc8df85f968
safia88 <safialiusa@gmail.com>
2019-11-27 12:56:32 -0500

    logout move to logout component

--- src/react/components/TopMenu.js
+++ src/react/components/TopMenu.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { Link } from ".";
 import "./TopMenu.css";
-import { withAsyncAction } from "../HOCs";
 import pages from "../pages";
-
+import { LogOut } from "./index";
 import { Menu, Icon } from 'antd';
 import 'antd/dist/antd.css';
-
 
 class TopMenu extends React.Component {
   state = {
@@ -20,56 +18,42 @@ class TopMenu extends React.Component {
     });
   };
 
-  handleLogout = event => {
-    event.preventDefault();
-    this.props.logout();
-  };
-
-  
-
   render() {
     return (
       <div id="menu">
-        {/* <h1>Urth Kwitter</h1> */}
         <Menu onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal">
-        <Menu.Item key="home" >
-          <div id="homeButton">
-            <Icon type="home" />
-            <Link to={pages.Home.path}>Home</Link>
-          </div>
-        </Menu.Item>
-
-        <Menu.Item key="users" > 
-          <div id="usersButton">
-            <Icon type="heart" />
-            <Link to={pages.Users.path}>Users</Link>
-          </div>
-        </Menu.Item>
-
-        <Menu.Item key="setting" > 
-          <div id="settingsButton">
-            <Icon type="setting" />
-            <Link to={pages.Settings.path}>Settings</Link>
-          </div>
-        </Menu.Item>
-
-        <Menu.Item key="logout">
-          {this.props.isAuthenticated && (
-            <div id="logout">
-              <Icon type="logout" />
-              <Link to="/" onClick={this.handleLogout}>
-                Logout
-              </Link>
+          <Menu.Item key="home" >
+            <div id="homeButton">
+              <Icon type="home" />
+              <Link to={pages.Home.path}>Home</Link>
             </div>
-          )}
-        </Menu.Item>
-      </Menu>
-       
+          </Menu.Item>
+
+          <Menu.Item key="users" >
+            <div id="usersButton">
+              <Icon type="heart" />
+              <Link to={pages.Users.path}>Users</Link>
+            </div>
+          </Menu.Item>
+
+          <Menu.Item key="setting" >
+            <div id="settingsButton">
+              <Icon type="setting" />
+              <Link to={pages.Settings.path}>Settings</Link>
+            </div>
+          </Menu.Item>
+
+          <Menu.Item key="logout">
+            {this.props.isAuthenticated && (
+              <LogOut />
+            )}
+          </Menu.Item>
+        </Menu>
+
       </div>
     );
   }
 }
 
+export default TopMenu;
 
-
-export default withAsyncAction("auth", "logout")(TopMenu);
