d8327d0772ec4106193df577ef18aa2f4499f0a8
Samantha Kyle <samanthakyle@gmail.com>
2019-12-04 15:02:46 -0500

    style not found page

--- src/react/pages/NotFound.js
+++ src/react/pages/NotFound.js
@@ -1,14 +1,21 @@
 import React from "react";
 import { Link } from "../components";
 import pages from "../pages";
+import { Result, Button } from 'antd';
+import 'antd/dist/antd.css';
+import "./Style.css";
 
 class NotFound extends React.Component {
   render() {
     return (
-      <>
-        <p>Page not found for {this.props.location.pathname}</p>
-        <Link to={pages.Home.path}>Go Home</Link>
-      </>
+      <Result 
+      status="404" 
+      title="404" 
+      subTitle="Sorry, the page you visited does not exist...yet">
+      <Button type="primary" class="goback">
+        <Link to={pages.Home.path}>Go back to your live feed</Link>
+      </Button>
+    </Result>
     );
   }
 }
