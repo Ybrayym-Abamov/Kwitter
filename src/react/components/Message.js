845ff77abbc183d7adf094b33a48c3fa46968035
Samantha Kyle <samanthakyle@gmail.com>
2019-12-09 14:19:10 -0500

    styling update

--- src/react/components/Message.js
+++ src/react/components/Message.js
@@ -51,7 +51,7 @@ class Message extends Component {
                             </Card>
                         ))}
                     </div>
-
+                <h3>there are no more messages</h3>
                 </div>
             )
         }
