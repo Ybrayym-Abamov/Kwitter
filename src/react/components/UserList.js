f27124f7b4d50f62aff06a86fd6778810dd1b7d5
Ybrayym <ybrayymjan90@gmail.com>
2019-12-10 09:33:36 -0500

    not much

--- src/react/components/UserList.js
+++ src/react/components/UserList.js
@@ -1,7 +1,13 @@
 import React from "react";
-// import { UserInfoBlock } from ".";
-// import { Link } from ".";
+// // import { UserInfoBlock } from ".";
+// // import { Link } from ".";
 // import "./UserList.css";
+// import { Card, Avatar } from 'antd';
+// import 'antd/dist/antd.css';
+
+// const { Meta } = Card;
+
+
 
 class UserList extends React.Component {
     render() {
@@ -19,3 +25,55 @@ class UserList extends React.Component {
       }
 }
 export default UserList;
+// class UserList extends React.Component {
+
+//   state = {
+//     items: [],
+//     isLoaded: false,
+//   }
+
+
+//   componentDidMount() {
+//     this.props.getUserMessages(this.props.username);
+//     this.messagePollingID = setInterval(this.props.getUserMessages, 5000);
+//   }
+
+//   componentWillUnmount() {
+//     clearInterval(this.messagePollingID)
+//   }
+
+
+//   render() {
+
+//     if (this.props.result === null) {
+//       return <div>Loading...</div>;
+//     }
+//     const getUsers = this.props.result.messages
+
+
+//     return (
+
+//       <div className='Message'>
+//         <h3>Latest Kweets</h3>
+//         <div>
+//           {getMessages.map(item => (
+//             <Card key={item.id} className="userpost">
+//               <div className="postinfo">
+//                 <Meta key={item.id}
+//                   avatar={<Avatar size={64} src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" />} //we'll need to make this link to whatever icon the user uploads
+//                   title={item.username}
+//                   description={item.text}
+//                 />
+//                 <div className="timestamp">{moment(item.createdAt).startOf('minutes').fromNow()}</div>
+//               </div>
+//             </Card>
+//           ))}
+//         </div>
+
+//       </div>
+//     )
+//   }
+// }
+
+
+// export default withAsyncAction("messages", "getUserMessages")(Message);
